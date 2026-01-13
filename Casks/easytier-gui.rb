9546c2edfae19253aaef495bc993c3934c33a1ba
Ch3n4y <ch3n4y@gmail.com>
2026-01-13 22:55:16 +0800

    Optimize formula and cask, add .gitignore

--- Casks/easytier-gui.rb
+++ Casks/easytier-gui.rb
@@ -1,15 +1,10 @@
 cask "easytier-gui" do
   version "2.5.0"
 
-  on_arm do
-    url "https://github.com/EasyTier/EasyTier/releases/download/v#{version}/easytier-gui_#{version}_aarch64.dmg"
-    sha256 "040c6741af07ebb469048b0cfa3fdd4215b00d31cc5539a9b46acfbce0737d4a"
-  end
-  on_intel do
-    url "https://github.com/EasyTier/EasyTier/releases/download/v#{version}/easytier-gui_#{version}_x64.dmg"
-    sha256 "b741fbd8b48985528a695bee1880c8029fbe41d61acf44138d3d86706b04a8d7"
-  end
+  sha256 arm:   "040c6741af07ebb469048b0cfa3fdd4215b00d31cc5539a9b46acfbce0737d4a",
+         intel: "b741fbd8b48985528a695bee1880c8029fbe41d61acf44138d3d86706b04a8d7"
 
+  url "https://github.com/EasyTier/EasyTier/releases/download/v#{version}/easytier-gui_#{version}_#{Hardware::CPU.intel? ? "x64" : "aarch64"}.dmg"
   name "EasyTier GUI"
   desc "GUI client for EasyTier - a decentralized mesh VPN solution"
   homepage "https://github.com/EasyTier/EasyTier"
--- Formula/easytier.rb
+++ Formula/easytier.rb
@@ -4,14 +4,12 @@ class Easytier < Formula
   version "2.5.0"
   license "LGPL-3.0"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/EasyTier/EasyTier/releases/download/v2.5.0/easytier-macos-aarch64-v2.5.0.zip"
-      sha256 "ce3744470e41675358728ab0a8da798436ec763f561d8b698d8d06a7ffa21895"
-    else
-      url "https://github.com/EasyTier/EasyTier/releases/download/v2.5.0/easytier-macos-x86_64-v2.5.0.zip"
-      sha256 "9bc12142f8808f0de02575064e39901ac6804d82ef27c1d08ecf0bced3e79c47"
-    end
+  if Hardware::CPU.arm?
+    url "https://github.com/EasyTier/EasyTier/releases/download/v2.5.0/easytier-macos-aarch64-v2.5.0.zip"
+    sha256 "ce3744470e41675358728ab0a8da798436ec763f561d8b698d8d06a7ffa21895"
+  else
+    url "https://github.com/EasyTier/EasyTier/releases/download/v2.5.0/easytier-macos-x86_64-v2.5.0.zip"
+    sha256 "9bc12142f8808f0de02575064e39901ac6804d82ef27c1d08ecf0bced3e79c47"
   end
 
   livecheck do
@@ -23,71 +21,74 @@ class Easytier < Formula
   def install
     bin.install "easytier-core", "easytier-cli", "easytier-web", "easytier-web-embed"
 
-    # Create config directory
+    # Generate default configuration
     (etc/"easytier").mkpath
+    (etc/"easytier/default.conf").write default_config unless (etc/"easytier/default.conf").exist?
+  end
 
-    # Generate default config file if it doesn't exist
-    default_config = etc/"easytier/default.conf"
-    unless default_config.exist?
-      default_config.write <<~TOML
-        instance_name = "default"
-        dhcp = true
-        listeners = [
-            "tcp://0.0.0.0:11010",
-            "udp://0.0.0.0:11010",
-            "wg://0.0.0.0:11011",
-            "ws://0.0.0.0:11011/",
-            "wss://0.0.0.0:11012/",
-        ]
-        exit_nodes = []
-        rpc_portal = "0.0.0.0:0"
+  def default_config
+    <<~TOML
+      instance_name = "default"
+      dhcp = true
+      listeners = [
+          "tcp://0.0.0.0:11010",
+          "udp://0.0.0.0:11010",
+          "wg://0.0.0.0:11011",
+          "ws://0.0.0.0:11011/",
+          "wss://0.0.0.0:11012/",
+      ]
+      exit_nodes = []
+      rpc_portal = "0.0.0.0:0"
 
-        [[peer]]
-        uri = "tcp://public.easytier.top:11010"
+      [[peer]]
+      uri = "tcp://public.easytier.top:11010"
 
-        [network_identity]
-        network_name = "default"
-        network_secret = "default"
+      [network_identity]
+      network_name = "default"
+      network_secret = "default"
 
-        [flags]
-        default_protocol = "udp"
-        dev_name = ""
-        enable_encryption = true
-        enable_ipv6 = true
-        mtu = 1380
-        latency_first = false
-        enable_exit_node = false
-        no_tun = false
-        use_smoltcp = false
-        foreign_network_whitelist = "*"
-        disable_p2p = false
-        p2p_only = false
-        relay_all_peer_rpc = false
-        disable_tcp_hole_punching = false
-        disable_udp_hole_punching = false
-      TOML
-    end
+      [flags]
+      default_protocol = "udp"
+      dev_name = ""
+      enable_encryption = true
+      enable_ipv6 = true
+      mtu = 1380
+      latency_first = false
+      enable_exit_node = false
+      no_tun = false
+      use_smoltcp = false
+      foreign_network_whitelist = "*"
+      disable_p2p = false
+      p2p_only = false
+      relay_all_peer_rpc = false
+      disable_tcp_hole_punching = false
+      disable_udp_hole_punching = false
+    TOML
   end
 
   def caveats
     <<~EOS
-      EasyTier has been installed with the following commands:
-        - easytier-core: Core VPN service
-        - easytier-cli: Command-line interface
-        - easytier-web: Web console server
-        - easytier-web-embed: Embedded web server
+      EasyTier has been installed with the following binaries:
+        • easytier-core       - Core VPN service
+        • easytier-cli        - Command-line interface
+        • easytier-web        - Web console server
+        • easytier-web-embed  - Embedded web server
 
-      Configuration file location:
-        #{etc}/easytier/default.conf
+      Configuration:
+        Default config: #{etc}/easytier/default.conf
+        Edit the config file to customize your network settings.
 
-      To start easytier-core as a background service:
-        sudo brew services start easytier
+      Service Management:
+        Start service:  sudo brew services start easytier
+        Stop service:   sudo brew services stop easytier
+        Restart:        sudo brew services restart easytier
+        Status:         brew services info easytier
 
-      To stop the service:
-        sudo brew services stop easytier
-
-      To run manually:
+      Manual Usage:
         sudo easytier-core -c #{etc}/easytier/default.conf
+
+      Documentation:
+        https://github.com/EasyTier/EasyTier
     EOS
   end
 
@@ -101,6 +102,16 @@ class Easytier < Formula
   end
 
   test do
+    # Test version output
     assert_match version.to_s, shell_output("#{bin}/easytier-core --version 2>&1")
+
+    # Test that all binaries exist and are executable
+    %w[easytier-core easytier-cli easytier-web easytier-web-embed].each do |binary|
+      assert_predicate bin/binary, :exist?
+      assert_predicate bin/binary, :executable?
+    end
+
+    # Test config file was created
+    assert_predicate etc/"easytier/default.conf", :exist?
   end
 end
